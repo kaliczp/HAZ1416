546f35e997097d33b836a09309bac219de9263cd
Péter Kalicz <kaliczp@gmail.com>
2021-01-16 10:19:36 +0100

    Filter out name of redundant files
    Theoretically csakadatki files contain data wich ara available other files.

--- Readme.R
+++ Readme.R
@@ -1,5 +1,6 @@
 ttyear <- 2014
 dircontent <- dir(path = paste0("./", ttyear, "/konvertalt"))
+dircontent <- dircontent[grep("csak", dircontent, invert = TRUE)]
 
 ## Submodule added
 ## git submodule add https://github.com/kaliczp/smartbe
