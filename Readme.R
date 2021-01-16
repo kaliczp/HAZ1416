f6b340dfcdb90a7d568a95d2edefeb366da92521
Péter Kalicz <kaliczp@gmail.com>
2021-01-16 10:07:14 +0100

    smartbe() data processing function added
    smartbe() function added through its git repository as a git submodule. It is sourced in the script.

--- Readme.R
+++ Readme.R
@@ -1,2 +1,7 @@
 ttyear <- 2014
 dircontent <- dir(path = paste0("./", ttyear, "/konvertalt"))
+
+## Submodule added
+## git submodule add https://github.com/kaliczp/smartbe
+
+source("smartbe/smartbe.R")
