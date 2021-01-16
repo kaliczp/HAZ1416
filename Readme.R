c18fcfefa59b63c41b1b72cb49b9324e9f781f3a
Péter Kalicz <kaliczp@gmail.com>
2021-01-16 10:28:27 +0100

    Filenames are in a data.frame wiht its year
    For better processing a data.frame organised with the filename and the year.

--- Readme.R
+++ Readme.R
@@ -1,6 +1,8 @@
 ttyear <- 2014
 dircontent <- dir(path = paste0("./", ttyear, "/konvertalt"))
-dircontent <- dircontent[grep("csak", dircontent, invert = TRUE)]
+dircontent.df <- data.frame(year = ttyear,
+                            fname = dircontent[grep("csak", dircontent, invert = TRUE)]
+                            )
 
 ## Submodule added
 ## git submodule add https://github.com/kaliczp/smartbe
