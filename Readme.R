a7a1ba6c18f3d2bdf89ff98336c46326a1c2bcef
Péter Kalicz <kaliczp@gmail.com>
2021-01-17 07:49:49 +0100

    fulldir.df created for all years
    Now a database is built with all neccessary filenames.
    There are some mentions (megjegyzes) file, which are excluded.
    
    There is a potential possibility to treate different channel setting of the instrument individually.
    
    This commit closes #1 issue.

--- Readme.R
+++ Readme.R
@@ -1,12 +1,21 @@
-ttyear <- 2014
-dircontent <- dir(path = paste0("./", ttyear, "/konvertalt"))
-dircontent.df <- data.frame(year = ttyear,
-                            fname = dircontent[grep("csak", dircontent, invert = TRUE)]
-                            )
+fulldir.df <- data.frame(year = numeric(), fname = character())
+for(ttyear in 2014:2016) {
+    dircontent <- dir(path = paste0("./", ttyear, "/konvertalt"))
+    ttnocsak <- dircontent[grep("csak", dircontent, invert = TRUE)]
+    ttnomegj <- ttnocsak[grep("megj", ttnocsak, invert = TRUE)]
+    dircontent.df <- data.frame(year = ttyear,
+                                fname = ttnomegj
+                                )
+    fulldir.df <- rbind(fulldir.df, dircontent.df)
+}
+
+different.setup <- 1
+fulldir.df <- cbind(fulldir.df, channel = 1)
+fulldir.df[different.setup, "channel"]  <- 0
 
 ## Submodule added
 ## git submodule add https://github.com/kaliczp/smartbe
 
 source("smartbe/smartbe.R")
-smartbe(paste0(dircontent.df[1, "year"], "/konvertalt/", dircontent.df[1, "fname"]), channel=0)
-smartbe(paste0(dircontent.df[1, "year"], "/konvertalt/", dircontent.df[2, "fname"]))
+smartbe(paste0(fulldir.df[1, "year"], "/konvertalt/", fulldir.df[1, "fname"]), channel = fulldir.df[1, "channel"])
+smartbe(paste0(fulldir.df[1, "year"], "/konvertalt/", fulldir.df[2, "fname"]), channel = fulldir.df[1, "channel"])
